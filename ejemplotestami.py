75dc45fb185e40a7830c8e60e390e6a80e957472
LuisRas99 <103764687+LuisRas99@users.noreply.github.com>
2022-05-25 19:29:33 -0600

    Final del dia
    Se terminaron los capítulos pendientes y se continuo el programa, casi a punto de terminar.

--- ejemplotestami.py
+++ ejemplotestami.py
@@ -1,7 +1,19 @@
 import socket
+import time
 
 print("Iniciando programa")
 s=socket.socket()
 s.connect(("172.17.207.207",5038))
+
+print("Mandando Login")
+s.send ("Action:Login\n".encode())
+s.send ("Username:diagnocons\n".encode())
+s.send ("Secret:diagnocons2022\n".encode())
+s.send ("\n".encode())
+time.sleep(8)
+
+print("Mandando Logoff")
+s.send ("Action:Logoff\n".encode())
+s.send ("\n".encode())
 
 print("Fin del programa")
