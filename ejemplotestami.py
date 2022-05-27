5675f7212b288c24eb413842999789416e996610
LuisRas99 <103764687+LuisRas99@users.noreply.github.com>
2022-05-26 19:20:21 -0600

    Final del dia
    Se avanzo hasta el capitulo click to dial

--- ejemplotestami.py
+++ ejemplotestami.py
@@ -1,19 +1,45 @@
 import socket
 import time
+from tkinter import *
 
-print("Iniciando programa")
+def iniciar_ami(user,password):
+    s.send ("Action:Login\n".encode())
+    comando = "Username:"+user+"\n"
+    s.send (comando.encode())
+    comando = "Secret:"+password+"\n"
+    s.send (comando.encode())
+    s.send ("\n".encode())
+
+def llamar(numero):
+    s.send ("Action:Originate\n".encode())
+    comando = "Channel:SIP/"+str(numero)+"\n"
+    s.send (comando.encode())
+    s.send ("Context:prueba\n".encode())
+    s.send ("Exten:902\n".encode())
+    s.send ("Priority:1\n".encode())
+    s.send ("\n".encode())
+    
+def finalizar_ami():
+    s.send ("Action:Logoff\n".encode())
+    s.send ("\n".encode())
+
+def revisar():
+    llamar(200)
+
 s=socket.socket()
 s.connect(("172.17.207.207",5038))
+ventana = Tk() 
+ventana.geometry("380x300")
+iniciar_ami("diagnocons","diagnocons2022")
 
-print("Mandando Login")
-s.send ("Action:Login\n".encode())
-s.send ("Username:diagnocons\n".encode())
-s.send ("Secret:diagnocons2022\n".encode())
-s.send ("\n".encode())
+boton = Button(ventana,text="Llamar",command=revisar) 
+boton.pack()
+ventana.mainloop()
+
+print("Iniciando Login")
 time.sleep(8)
-
 print("Mandando Logoff")
-s.send ("Action:Logoff\n".encode())
-s.send ("\n".encode())
+finalizar_ami()	
+
 
 print("Fin del programa")
